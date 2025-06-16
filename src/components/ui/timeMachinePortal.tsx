6c7fc89d17bdeb379b4fff5d2b7e373e7d66273a
gurnish-singh <gsingh11_be17@thapar.edu>
2025-06-16 18:13:20 +0530

    patch

--- src/components/ui/timeMachinePortal.tsx
+++ src/components/ui/timeMachinePortal.tsx
@@ -1,8 +1,9 @@
+import tardis from 'src/assets/images/telephone_booth.gif';
 export default function TimePortal() {
   return (
     <div className="group">
       <img
-        src="src/assets/images/telephone_booth.gif" // use from /public instead of src/assets for public paths
+        src={tardis} // use from /public instead of src/assets for public paths
         alt="Time Travel Portal"
         className="w-20 h-auto cursor-pointer transition-transform duration-300 group-hover:scale-110"
         onClick={() =>
