94e6cf6899120a6567e56f18f614f322c21dc012
gurnish-singh <gsingh11_be17@thapar.edu>
2025-06-16 15:42:58 +0530

    now it should work

--- src/lib/medium.ts
+++ src/lib/medium.ts
@@ -1,6 +1,5 @@
-
 export async function getMediumPosts() {
-  const response = await fetch('/api/medium');
+  const response = await fetch('https://medium-rss-api.onrender.com/medium');
   if (!response.ok) {
     throw new Error('Failed to fetch Medium posts');
   }
